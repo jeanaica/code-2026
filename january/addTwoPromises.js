7d3cb918737b8d00188c801703907d17f6427cf0
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2026-01-10 22:12:40 +0800

    Solve add two promises

---
+++ january/addTwoPromises.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+  const res1 = await promise1;
+  const res2 = await promise2;
+
+  return res1 + res2;
+};
