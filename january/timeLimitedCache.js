7c3433eddc8cc7af451306f3c274eeb081d379bc
Jeanaica Alinsub <sjeanaica@gmail.com>
2026-01-18 09:25:30 +0800

    Add TimeLimitedCache class with basic functionality
    Implement a time-limited cache with set, get, and count methods.

---
+++ january/timeLimitedCache.js
@@ -0,0 +1,53 @@
+var TimeLimitedCache = function() {
+    this.map = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const exists = this.map.has(key);
+    
+    if (exists) {
+        const oldTimerId = this.map.get(key).timerId
+        clearTimeout(oldTimerId);
+    }
+    
+    const newTimerId = setTimeout(() => {
+        this.map.delete(key);
+    }, duration);
+    
+    this.map.set(key, {
+        value: value,
+        timerId: newTimerId
+    });
+    
+    return exists;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    const exists = this.map.has(key);
+
+    return exists ? this.map.get(key).value : -1
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.map.size
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
